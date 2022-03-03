fda9ff3ed3c6ee772b1f687f0584411e1655acb3
Psybex <dkvowk@gmail.com>
2022-03-03 10:16:02 +0900

    상속

--- src/main.js
+++ src/main.js
@@ -1,26 +1,23 @@
-// @ts-check
-/* eslint-disable */
+// Prototype
 
-const { count } = require('console')
-
-var numCounters = 0
-
-function getCounter() {
-  numCounters += 1
-
-  var result = { count: count, total: 0 }
-  function count() {
-    result.total += 1
-  }
-  return result
+function Person(name, age) {
+  this.name = name
+  this.age = age
 }
 
-var counterA = getCounter()
-counterA.count()
-counterA.count()
+Person.prototype.greet = function greet() {
+  return `Hi, ${this.name}!`
+}
 
-var counterB = getCounter()
-counterB.count()
-counterB.count()
+function Student(name, age) {
+  this.__proto__.constructor(name)
+}
 
-console.log(counterA.total, counterB.total, numCounters)
+Student.prototype.study = function study() {
+  return `${this.name} is studying.`
+}
+
+Object.setPrototypeOf(Student.prototype, Person.prototype) // Person -> Student 상속
+
+const me = new Student('gapjin', 35) // new를 통해서 함수를 부를 경우 생성자 역할을 한다.
+console.log(me.greet())
